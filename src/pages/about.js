75614246a21b3b90706fe382276bf9169fa01f60
thaitwo <tmtu90@gmail.com>
2019-04-15 23:05:39 -0700

    Add image and text content to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Image } from "../components/Image.js"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,8 +11,18 @@ const AboutPage = () => (
     <SEO title="Page two" />
 
     <Header pageTitle="About" bgFile="myanmar.jpg"/>
-
-    <Link to="/">Go back to the homepage</Link>
+    <div class="container">
+      <div style={{display: `flex`, alignItems: `center`}}>
+        <div style={{flex: `1`, paddingRight: `64px`}}>
+          <Image filename="thai.jpg" />
+        </div>
+        <div style={{flex: `2`}}>
+          <p>Welcome to my photography website! I'm Thai, a photography enthusiast living in Northern California.</p>
+          <p>I got into photography back in 2013 when I bought my first DSLR camera. Ever since, capturing majestic sceneries and memorable life moments have been a hobby and passion of mine.</p>
+          <p>Aside from photography, I also enjoy playing tennis and basketball, outdoor activities, building great web products, and traveling.</p>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
